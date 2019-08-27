91dd25db6471f716afe012a2385ab2f0740216d7
Jarred-528 <jarredanderson@unomaha.edu>
2019-08-27 10:22:17 -0500

    First commit of project

---
+++ Test.py
@@ -0,0 +1,20 @@
+x = 10
+y = 7
+
+while x > 8:
+    print(x, "is greater")
+    x -= 1
+
+    if x <= 9:
+        print("The number is", x)
+        x -= 1
+
+print("Hello, my name is Jarred.")
+
+
+
+
+
+
+
+
